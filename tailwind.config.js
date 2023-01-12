18551214b08c0d59bf880c55040511c047cc5ecc
khramov23 <79625109971@ya.ru>
2023-01-13 01:02:41 +0300

    add tailwind small config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors")
+const plugin = require("tailwindcss/plugin")
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: '#ff3838',
+      black: colors.black,
+      white: colors.white,
+      transparent: colors.transparent,
+      red: {
+        'light': '#ff4d4d',
+        'dark': '#E3000E'
+      },
+    },
     extend: {},
   },
   plugins: [],
